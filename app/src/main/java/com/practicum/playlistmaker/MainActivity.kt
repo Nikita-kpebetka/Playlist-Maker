cc5c39a5f907416bdac9db7ddc357b028276210f
Nikita <kbetkabmope@gmail.com>
2026-04-01 19:13:46 +0300

    добавление "Toast" на главный экран

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,24 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchButton = findViewById<Button>(R.id.magnifier)
+        searchButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "БУУУУУУ!", Toast.LENGTH_SHORT).show()
+        }
+
+        val mediaButton = findViewById<Button>(R.id.media_library)
+        mediaButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Ха-Ха Испугался?", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        val settingsButton = findViewById<Button>(R.id.settings)
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Не бойся, я друг)))", Toast.LENGTH_SHORT).show()
         }
     }
 }
