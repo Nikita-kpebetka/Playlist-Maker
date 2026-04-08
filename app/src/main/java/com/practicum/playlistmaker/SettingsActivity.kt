5a9cf50f4147a5a45ab74e4d27194a822150cdc6
Nikita <kbetkabmope@gmail.com>
2026-04-08 21:08:52 +0300

    Реализация кнопок настроек («Поделиться приложением», «Настройки», «Пользовательское соглашение»)

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,8 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,6 +15,36 @@ class SettingsActivity : AppCompatActivity() {
         val backButton = findViewById<ImageView>(R.id.backButton)
         backButton.setOnClickListener {
             finish()
+        }
+
+        val shareAppButton = findViewById<FrameLayout>(R.id.shareApp)
+        shareAppButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.Share_click))
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.Share_app)))
+        }
+
+        val writeSupportButton = findViewById<FrameLayout>(R.id.writeSupport)
+        writeSupportButton.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = android.net.Uri.parse("mailto:${getString(R.string.My_email)}")
+            emailIntent.putExtra(
+                Intent.EXTRA_SUBJECT,
+                "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            )
+            emailIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Спасибо разработчикам и разработчицам за крутое приложение!"
+            )
+            startActivity(emailIntent)
+        }
+
+        val userAgreementButton = findViewById<FrameLayout>(R.id.userAgreement)
+        userAgreementButton.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW)
+            browserIntent.data = android.net.Uri.parse("https://yandex.ru/legal/practicum_offer/ru/")
+            startActivity(browserIntent)
         }
     }
 }
